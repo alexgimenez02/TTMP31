fdfe48c4ac87ed3c9e4fd25622b3b13ed30b0595
Sofía Cárdenas <65339757+SofiaCardenasLinares@users.noreply.github.com>
2022-05-11 00:42:11 +0200

    Change in ElectricGuitarTest.py

--- src/ElectricGuitarTest.py
+++ src/ElectricGuitarTest.py
@@ -4,7 +4,7 @@ from music import *
 if "__main__" in __name__:
    
 
-   instr = secondaryGuitar("Guitar test", 150)
+   instr = electricGuitar("Guitar test", 150)
     
    pitches = [G5, G5, E5, D5, SILENT, D5, DS5, D5, C5, D5]
    duration = [HN, QN, QN, DHN, QN, EN, EN, EN, HN, HN]
