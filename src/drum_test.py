c4965c3d48e6957696a77deee3f3c241d58a999b
GerardBarriga <99078428+GerardBarriga@users.noreply.github.com>
2022-05-11 11:12:58 +0200

    Additions to drum_test

--- src/drum_test.py
+++ src/drum_test.py
@@ -20,11 +20,28 @@ def test():
 
    time.sleep(1.0)
    
-   #Change Tempo test and add Snare
+   #Change Tempo and add Snare
    drum.changeTempo(50.0)
    snarePitches   = [REST, SNR] * 4
    snareDurations = [QN,   QN] * 4
    drum.addToPhrase(snarePitches, snareDurations)
    drum.playTheme()
+   
+   time.sleep(10.0)
+   
+   #Change tempo again and add the Hi Hat
+   drum.changeTempo(125.0)
+   hiHatPitches   = [CHH] * 15 + [OHH]
+   hiHatDurations = [EN] * 15  + [EN]
+   drum.addToPhrase(hiHatPitches, hiHatDurations)
+   drum.playTheme()
+   
+   time.sleep(7.0)
+   
+   #Loop what we have in order to produce the base.
+   Mod.repeat(drum.score, 8)
+   drum.playTheme()
+   
+
    
 test()
