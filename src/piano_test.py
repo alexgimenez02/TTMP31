18ee7c3d1a6883845b629bffd193945c182e6a4b
magmarc9 <marc.alcocer01@estudiant.upf.edu>
2022-05-11 10:54:32 +0200

    Updated piano_test.py
    - Improved the piano test

--- src/piano_test.py
+++ src/piano_test.py
@@ -5,17 +5,38 @@ from music import *
 def test():
     piano = Piano("Piano test", 80)
 
-    l_pitches = [C4, E4, G4, C4]
-    l_durations = [QN, QN, QN, QN]
-
-    r_pitches = [[C3, E5, G5]]
-    r_durations = [WN]
+    l_pitches = [[C3, E3, G3]]
+    l_durations = [WN]
+ 
+    r_pitches = [C4, E4, G4, C4]
+    r_durations = [QN, QN, QN, QN]    
 
     piano.addPhraseTwoHands(l_pitches, l_durations, r_pitches, r_durations)
+   
 
-    pitches = [D4, E4, C4]
-    durations = [QN, QN, QN]
-    piano.addPhrase(pitches, durations)
+    l_pitches = [REST]
+    l_durations = [WN]
+    
+    r_pitches = [D4, E4, C4, REST]
+    r_durations = [QN, QN, QN, QN]
+    piano.addPhraseTwoHands(l_pitches, l_durations, r_pitches, r_durations)
+        
+    
+    r_pitches = [F4, A4, C5, C5]
+    r_durations = [QN, QN, QN, QN]
+
+    l_pitches = [[F3, A3, C4]]
+    l_durations = [WN]
+
+    piano.addPhraseTwoHands(l_pitches, l_durations, r_pitches, r_durations)
+    
+    r_pitches = [F4]
+    r_durations = [WN]
+
+    l_pitches = [REST]
+    l_durations = [WN]
+
+    piano.addPhraseTwoHands(l_pitches, l_durations, r_pitches, r_durations)
 
 
     piano.play_theme()
