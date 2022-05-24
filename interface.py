e1cea81a2602d3d31e754ee8a5ab10cb2ae5a6c7
DidacCabi <didac.cabiscol01@estudiant.upf.edu>
2022-05-24 20:54:43 +0200

    implementation interface
    Now the buttons of play, download and instruments checkboxes works, the volume slider also works

--- interface.py
+++ interface.py
@@ -1,10 +1,36 @@
 from music import *
 from gui import *
+from piano import Piano
+from electricguitar import electricGuitar
 
+pianoSelected = True
+electGuitarSelected = True
+score  = Score("Composition", 120)
 
 def mainSong():
-   print "Now the music would play..."
    #MAIN COMPOSITION
+   print "Playing..."
+   piano = Piano("Piano test", 120, [0, 1])
+   electGuitar = electricGuitar("Electric guitar", 120, 2)   
+   
+   pitches = [SILENT, A5,     G5,      E5, D5,     D5, DS5, D5, C5]
+   duration = [QN,    DHN,    WN,      QN, DHN,    SN, SN, DQN, DHN ]   
+   electGuitar.addToPhase(pitches, duration)
+   electGuitar.create_score();
+   
+   r_pitches = [[A3, C4, E4], [A3, C4, E4], [A3, C4, E4],     [B3, E4, G4], [B3, E4, G4], [B3, E4, G4]]
+   r_durations = [DQN,            DQN,            QN,              DQN,          DQN,           QN] 
+   l_pitches = [[A1, A2],        [A1, A2],     [A1, A2],         [E2, E3],    [E2, E3],      [E2, E3]]
+   l_durations = [DQN,             DQN,           QN,                DQN,        DQN,           QN]    
+   piano.addPhraseTwoHands(l_pitches, l_durations, r_pitches, r_durations, 2)    
+   piano.create_score();   
+   
+   if pianoCheck.isChecked():
+      score.addPart(piano.l_hand_part)
+      score.addPart(piano.r_hand_part)   
+   if cGuitarCheck.isChecked():
+      score.addPart(electGuitar.part)   
+   Play.midi(score)   
    
 
 #--------------INTERFACE PART-------------------------
@@ -24,7 +50,11 @@ d.add(playButton, 405,320)
 
 #volume
 def printValue(volume):
-   print volume   # replace this with whatever needs to happen
+   Play.setVolume(volume,0) 
+   Play.setVolume(volume,1) 
+   Play.setVolume(volume,2) 
+   print(volume)
+   #Mod.normalize(volume)   # replace this with whatever needs to happen
 volume = VFader(575, 300, 595, 380, 0, 100, 50, printValue)
 d.add(volume)
 
@@ -53,5 +83,6 @@ cGuitarCheck.check()
 #download button
 def downloadFunc():
    print("Downloading...")
+   Write.midi(score, "song.mid")
 downloadBut = Button("Download song", downloadFunc)
 d.add(downloadBut, 610,530)
---
+++ piano.py
@@ -0,0 +1,46 @@
+from music import *
+
+
+class Piano:
+    name = ""
+    bpm = 0
+    score = None
+    l_hand_part = None
+    r_hand_part = None
+
+    def __init__(self, name='Piano', bpm=60, part_num=[0, 1]):
+        self.name = name
+        self.score = Score(name, bpm)
+        self.l_hand_part = Part(PIANO, part_num[0])
+        self.r_hand_part = Part(PIANO, part_num[1])
+
+
+    def addPhrase(self, pitches, duration, n_repetitions, hand="r"):
+        phrase = Phrase()
+        phrase.addNoteList(pitches, duration)
+        if n_repetitions > 0:
+            Mod.repeat(phrase, n_repetitions)  # repeat first phrase n times
+        if hand == "l":
+            self.l_hand_part.addPhrase(phrase)
+        elif hand == "r":
+            self.r_hand_part.addPhrase(phrase)
+
+    def addPhraseTwoHands(self, l_pitches, l_duration, r_pitches, r_duration, n_repetitions=0):
+        self.addPhrase(l_pitches, l_duration, n_repetitions, "l")
+        self.addPhrase(r_pitches, r_duration, n_repetitions, "r")
+
+    def changeTempo(self, tempo):
+        pass
+
+    def create_score(self):
+        if self.l_hand_part.getSize() > 0:
+            self.score.addPart(self.l_hand_part)
+        if self.r_hand_part.getSize() > 0:
+            self.score.addPart(self.r_hand_part)
+
+    def write_mdi(self, path):
+        Write.midi(self.score, path)
+
+    def play_theme(self):
+        self.create_score()
+        Play.midi(self.score)
