507c5f4ce5084e4bda3bea018b238d79784b5cf6
alexgimenez02 <48121702+alexgimenez02@users.noreply.github.com>
2022-05-25 12:17:12 +0200

    Some junk files erased
    For you, gamers

--- jythonMusic/1. playNote.py
+++
@@ -1,6 +0,0 @@
-# playNote.py
-# Demonstrates how to play a single note.
-from music import *        # import music library
- 
-note = Note(C4,HN)        # create a middle C half note
-Play.midi(note)            # and play it!
