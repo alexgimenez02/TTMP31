39d5b157c59da78fc676cd2b8f347571f7d243c5
Haylo <javier.serrano04@estudiant.upf.edu>
2022-05-31 05:01:25 +0200

    Pruebas

--- src/BassTest.py
+++ src/BassTest.py
@@ -5,17 +5,11 @@ from music import *
 def test():
     bass = Bass("Bass test", 80)
 
-    bassPitches1   = [REST, A2, REST, A2, E2, D2, REST, D2, A2, G2, REST, G2, D2, C2] 
-    bassPitches2   = [REST, C2, G2, FS2, REST, FS2, C2, B1, REST, B1, FS2, E2] 
-    bassPitches3   = [REST, E2, E2, B1, E2, REST]
+    bassPitch   = [REST, AS0, REST, C1, EF1, C1, REST, AS0, D2, G2, REST, G2, D2, AS0] 
     
-    bassDurations1 = [WN, QN, EN, EN, HN, QN, EN, EN, HN, QN, EN, EN, HN, QN] 
-    bassDurations2 = [EN, EN, HN, QN, EN, EN, HN, QN, EN,EN, HN, QN]
-    bassDurations3 = [EN, EN, QN, QN, HN, HN]
+    bassDuration = [WN, QN, EN, EN, HN, QN, EN, EN, HN, QN, EN, EN, HN, QN] 
 
-    bass.addToPhase(bassPitches1, bassDurations1)  
-    bass.addToPhase(bassPitches2, bassDurations2)
-    bass.addToPhase(bassPitches3, bassDurations3)
+    bass.addToPhase(bassPitch, bassDuration)  
 
     bass.playTheme()
 
