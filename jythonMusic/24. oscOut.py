d20a13c964a9bccab3342a183aa08e5c215770ad
Àlex Giménez <alex.gimenez02@estudiant.upf.edu>
2022-06-26 17:13:42 +0200

    Examples deleted

--- jythonMusic/24. oscOut.py
+++
@@ -1,9 +0,0 @@
-# oscOut.py
-
-from osc import *
-
-oscOut = OscOut("localhost", 57110) 
-
-oscOut.sendMessage("/helloWorld")
-oscOut.sendMessage("/test", 1, 2, 3)
-oscOut.sendMessage("/itsFullOfStars", 1, 2.35, "wow!", True)
