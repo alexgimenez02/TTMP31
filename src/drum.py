95416815011c07c1fe5ebaef1e79ae69279dfd82
Àlex Giménez <alex.gimenez02@estudiant.upf.edu>
2022-06-26 17:32:16 +0200

    Instruments added

---
+++ src/drum.py
@@ -0,0 +1,40 @@
+from music import *
+
+class Drums:
+   bpm = 0
+   score = None
+   part = None
+   
+   def __init__(self, name='Drums', bpm=120.0, part_num = 0):
+      self.name = name
+      self.bpm = bpm
+      self.score = Score(name, bpm)
+      self.part = Part("Drums", part_num, 9)
+      
+   def playNote(self,note):
+      testPart = Part("Drums", 0, 9)
+      testPhrase = Phrase()
+      testPhrase.addNote(note)
+      testPart.addPhrase(testPhrase)      
+      Play.midi(testPart)
+      testPhrase.empty()
+      testPart.empty()
+    
+   def addPhrase(self,pitches,duration,startTime=None):
+      phrase = Phrase(startTime)
+      phrase.addNoteList(pitches, duration)
+      self.part.addPhrase(phrase)
+    
+   def changeTempo(self,tempo):
+      self.score = Score(self.name, tempo)
+    
+   def create_score(self):
+      if self.part.getSize() > 0:
+         self.score.addPart(self.part)
+
+   def play_theme(self):
+      self.create_score()
+      Play.midi(self.score)
+      
+   def write_mdi(self, path):
+      Write.midi(self.score, path)    
---
+++ src/piano_test.py
@@ -0,0 +1,18 @@
+from piano import Piano
+from music import *
+
+
+def test():
+    piano = Piano("Piano test", 120)
+
+    r_pitches = [[A3, C4, E4], [A3, C4, E4], [A3, C4, E4],     [B3, E4, G4], [B3, E4, G4], [B3, E4, G4]]
+    r_durations = [DQN,            DQN,            QN,              DQN,          DQN,           QN]
+ 
+    l_pitches = [[A1, A2],        [A1, A2],     [A1, A2],         [E2, E3],    [E2, E3],      [E2, E3]]
+    l_durations = [DQN,             DQN,           QN,                DQN,        DQN,           QN]    
+
+    piano.addPhraseTwoHands(l_pitches, l_durations, r_pitches, r_durations, 10)    
+
+    piano.play_theme()
+
+test()
